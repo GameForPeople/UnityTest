7f8de42c2f5af9d34399cbec965ed8ce07a66d9d
GameForPeople <koreagamemaker@gmail.com>
2018-09-03 01:09:59 +0900

    [DEV] Adjust "Projectile and ProjectileController class" // DEV_33
    [DEV_33] Projectile과 ProjectileController Class 구현 및 적용

---
+++ 2D_Platformer/Assets/Scenes/_6_InGameScene/Weapon/Projectile.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    new Rigidbody2D rigidbody;
+
+    int moveDir;
+    float newPosX;
+    Vector3 newMove;
+    public int moveCount = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    //void Update () {
+    //	
+    //}
+
+    //void FixedUpdate()
+    //{
+    //}
+
+    public void StartAttack(int InDir, Vector3 InPos)
+    {
+        transform.position = new Vector3(InPos.x, InPos.y + 0.1f, 1);
+
+        newPosX = moveDir * 3.0f;
+        newMove.Set(newPosX, 0, 0);
+        rigidbody.MovePosition(transform.position + newMove);
+
+        moveDir = InDir;
+
+        moveCount = 90;
+        StartCoroutine("ProjectileCoroutine");
+    }
+
+    void MoveLeftOrRight()
+    {
+        newPosX = moveDir * 15.0f * Time.deltaTime;
+        newMove.Set(newPosX, 0, 0);
+        //transform.position = new Vector3(newPosX, transform.position.y, transform.position.z);
+        rigidbody.MovePosition(transform.position + newMove);
+    }
+
+    IEnumerator ProjectileCoroutine()
+    {
+        while (moveCount > 0)
+        {
+            --moveCount;
+            MoveLeftOrRight();
+
+            yield return new WaitForSeconds(1.0f / 60.0f);
+        }
+        TurnOff();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log("Attach : " + other.gameObject.layer);
+
+        if (other.gameObject.layer == 9 || other.gameObject.layer == 10)
+        {
+            //other.GetComponent<Projectile>();
+            // 이거는 캐릭터꺼야.
+        }
+        else
+        {
+            // 여기서 if를 하나 추가해야해, 적일때 추가
+            TurnOff();
+        }
+    }
+
+    void TurnOff()
+    {
+        moveCount = 0;
+
+        rigidbody.MovePosition(new Vector2(-100, -100));
+    }
+}
---
+++ 2D_Platformer/Assets/Scenes/_6_InGameScene/Weapon/ProjectileController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+    GameObject[] fireballArr = new GameObject[5];
+    GameObject[] lightballArr = new GameObject[5];
+
+    // Use this for initialization
+    void Start () {
+        fireballArr[0] = GameObject.Find("FireBall_0");
+        fireballArr[1] = GameObject.Find("FireBall_1");
+        fireballArr[2] = GameObject.Find("FireBall_2");
+        fireballArr[3] = GameObject.Find("FireBall_3");
+        fireballArr[4] = GameObject.Find("FireBall_4");
+
+        lightballArr[0] = GameObject.Find("LightBall_0");
+        lightballArr[1] = GameObject.Find("LightBall_1");
+        lightballArr[2] = GameObject.Find("LightBall_2");
+        lightballArr[3] = GameObject.Find("LightBall_3");
+        lightballArr[4] = GameObject.Find("LightBall_4");
+    }
+
+    // Update is called once per frame
+    //void Update () {
+	//	
+	//}
+    public void AttackFireBall(int InDir, Vector3 InPos)
+    {
+        for(int i = 0; i < 5; ++i)
+        {
+            if(fireballArr[i].GetComponent<Projectile>().moveCount == 0)
+            {
+                fireballArr[i].GetComponent<Projectile>().StartAttack(InDir, InPos);
+                return;
+            }
+        }
+    }
+
+    public void AttackLightBall(int InDir, Vector3 InPos)
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            if (lightballArr[i].GetComponent<Projectile>().moveCount == 0)
+            {
+                lightballArr[i].GetComponent<Projectile>().StartAttack(InDir, InPos);
+                return;
+            }
+        }
+    }
+}
